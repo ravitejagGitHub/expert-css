1720f5bf4e4123f953b761e4c2aca9f25affff2a
Raviteja Giduturi <raviteja.giduturi@refinitiv.com>
2020-03-22 19:25:54 +0530

    scss buider

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const isDevelopment = true;
+const template = `./src/navigation/index.html`;
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      path: path.resolve(__dirname, "dist"),
+      template
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+    })
+  ]
+};
